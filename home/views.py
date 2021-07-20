3af3f36f89622794efdfa7ae168267368111e55a
Rohit ranjan <ranjanrohit812@gmail.com>
2021-07-21 00:02:36 +0530

    Creating Various routes and views

--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is Home Page")
+
+def about(request):
+    return HttpResponse("This is About Page")
+
+def services(request):
+    return HttpResponse("This is the Service page $")
+
+def contact(request):
+    return HttpResponse("This is the contact page")
