7c9e34c9dcbd22c74f38ff0dd9cf7680292f6fb3
Rohit ranjan <ranjanrohit812@gmail.com>
2021-07-23 22:07:14 +0530

    Storing response in database using models

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 
 
@@ -22,5 +23,12 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name =name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
     return render(request, 'contact.html')
     # return HttpResponse("This is the contact page")
